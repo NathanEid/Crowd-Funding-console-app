2d52b896a9a5dd8a51037ee8b808e26a12bd991e
NathanEid <40915944+NathanEid@users.noreply.github.com>
2022-12-15 18:00:31 +0200

    Update email for login

--- login.py
+++ login.py
@@ -12,7 +12,7 @@ def userLogin():
     count = 1
     for line in file:
         record = list(line.split(":"))
-        if email == str(record[1]) and password == str(record[4]):
+        if email == str(record[3]) and password == str(record[4]):
             print("==========================Login success==========================")
             projectsMenu.projectMenu(record[0])
             count = 0
